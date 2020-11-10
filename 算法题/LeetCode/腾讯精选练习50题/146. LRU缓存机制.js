a904741ba20d97513abf5db6159d438e7fdc00a2
001975 <wangrun01@zybank.com.cn>
2020-11-10 20:35:41 +0800

    some code

---
+++ 算法题/LeetCode/腾讯精选练习50题/146. LRU缓存机制.js
@@ -0,0 +1,38 @@
+/**
+ * 2020.11.09
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+ * 实现 LRUCache 类：
+ * LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
+ * 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.obj = {};
+  this.getKey = [];
+};
+
+LRUCache.prototype.get = function (key) {
+  let value = -1;
+  if (this.obj[key]) {
+    this.getKey.splice(this.getKey.indexOf(key), 1);
+    this.getKey.push(key);
+    value = this.obj[key];
+  }
+  return value;
+};
+
+LRUCache.prototype.put = function (key, value) {
+  let keyPosition = this.getKey.indexOf(key);
+  if (keyPosition > -1) {
+    this.getKey.splice(this.getKey.indexOf(key), 1);
+  } else {
+    if (Object.keys(this.obj).length >= this.capacity) {
+      delete this.obj[this.getKey[0]];
+      this.getKey.shift();
+    }
+  }
+  this.obj[key] = value;
+  this.getKey.push(key);
+};
