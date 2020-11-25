f3ceffea39e435eb791233f520f87d2ddecfdb7c
001975 <wangrun01@zybank.com.cn>
2020-11-25 20:55:36 +0800

    some code

---
+++ 算法题/LeetCode/精选Top面试题/104. 二叉树的最大深度.js
@@ -0,0 +1,11 @@
+/**
+ * 2020.11.16
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+var maxDepth = function (root) {
+  return root === null
+    ? 0
+    : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
