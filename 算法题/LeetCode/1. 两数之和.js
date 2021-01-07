be6f596050c135bdd0e14acb3b015196b7647214
wr <m13121280211@163.com>
2021-01-07 15:52:20 +0800

    2021-01-07

--- 算法题/LeetCode/1. 两数之和.js
+++ 算法题/LeetCode/1. 两数之和.js
@@ -1,22 +1,13 @@
 var twoSum = function (nums, target) {
   let len = nums.length;
-  let result = [];
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (nums[i] + nums[j] == target) {
-        result.push(i);
-        result.push(j);
-        return result;
-      }
+  const obj = {}
+  for(let index  =0;index<=nums.length;index++){
+    if(obj(target - nums[index])) {
+      return []
+    } else {
+      obj[]
     }
   }
-};
-// var twoSum = function (nums, target) {
-//   let obj = {};
-//   for (let i = 0; i < nums.length; i++) {
-//     if(obj[])
-//   }
-// };
 var nums = [-3, 4, 3, 90];
 var target = 0;
 console.log(twoSum(nums, target));
