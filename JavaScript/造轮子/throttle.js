e2d130a27e3f1663f6e99e8fd3fa68e643f3f16e
wr <m13121280211@163.com>
2021-09-25 17:26:32 +0800

    update

---
+++ JavaScript/造轮子/throttle.js
@@ -0,0 +1,10 @@
+function throttle(cd, wait = 300) {
+  var t = null;
+  return function (...args) {
+    if (t) return;
+    t = setTimeout(() => {
+      cd.apply(this, args);
+      t = null;
+    }, wait);
+  };
+}
