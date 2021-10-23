24985d7571271896522508cf142bad00b6398b23
wr <m13121280211@163.com>
2021-10-23 23:50:44 +0800

    mobx 相关

--- Mobx/mobx-app/src/App.jsx
+++ Mobx/mobx-app/src/App.jsx
@@ -1,8 +1,30 @@
 import { Component } from "react";
+import { observer } from "mobx-react";
 
+
+@observer
 class App extends Component {
+  increment = () => {
+    const { store } = this.props;
+    store.increment();
+  };
   render() {
-    return <div>App</div>;
+    const { store } = this.props;
+
+
+
+    return (
+      <div>
+        <h2>App</h2>
+        <div>count:{store.count}</div>
+        <p>
+          <button onClick={this.increment}>increment</button>
+        </p>
+        <div>Total：{store.totalPrice}</div>
+        <div>Total：{store.totalPrice}</div>
+        <div>Total：{store.totalPrice}</div>
+      </div>
+    );
   }
 }
 export default App;
