182e0cf449e881c4d5821e1c8f1f51a99e242443
wr <m13121280211@163.com>
2021-10-23 21:34:59 +0800

    init

--- Mobx/mobx-app/src/App.jsx
+++ Mobx/mobx-app/src/App.jsx
@@ -1,20 +1,8 @@
-import { observer } from "mobx-react";
 import { Component } from "react";
 
-@observer
 class App extends Component {
   render() {
-    const { store } = this.props;
-    console.log(store);
-    return (
-      <div>
-        <h1>App....</h1>
-        {store.count}
-        <p>
-          <button onClick={store.increment}>ADD</button>
-        </p>
-      </div>
-    );
+    return <div>App</div>;
   }
 }
 export default App;
--- Mobx/mobx-app/src/index.js
+++ Mobx/mobx-app/src/index.js
@@ -1,18 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-import { observable, action } from "mobx";
-
-// 1. 初始化 mobx 容器仓库
-class Store {
-  @observable count = 22;
-  @action.bound increment = () => {
-    console.log(this.count)
-    this.count++;
-  };
-}
-// 2. 在组件中使用 mobx 容器仓库
-
-// 3. 在组件中发起 action 修改容器仓库
-ReactDom.render(<App store={new Store()} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
