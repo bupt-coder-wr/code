234177acd90a7787bf55c9c4b8abe43576fa8f08
wr <m13121280211@163.com>
2021-10-25 10:13:06 +0800

    react extension

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import { Component } from "react";
+import A from "./component/5_Context";
 
 class App extends Component {
   render() {
-    return <div>App...</div>;
+    return (
+      <div>
+        <h2>App...</h2>
+        <A />
+      </div>
+    );
   }
 }
 export default App;
---
+++ React/my-react-app/src/component/2_lazyLoad/Loading/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ backgroundColor: "gray", color: "orange" }}>
+          Loading....
+        </h1>
+      </div>
+    );
+  }
+}
