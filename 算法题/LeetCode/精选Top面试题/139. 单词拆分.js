1ba75fb3b8aa8e12445c3cab8a037fdff5b8450e
wr <m13121280211@163.com>
2022-01-17 20:59:06 +0800

    update

--- 算法题/LeetCode/精选Top面试题/139. 单词拆分.js
+++ 算法题/LeetCode/精选Top面试题/139. 单词拆分.js
@@ -4,17 +4,17 @@
  * @return {boolean}
  */
 var wordBreak = function (s, wordDict) {
-  let n = s.length;
-  let set = new Set(wordDict);
-  let dp = Array(n + 1).fill(false);
-  dp[0] = true;
+  let n = s.length
+  let set = new Set(wordDict)
+  let dp = Array(n + 1).fill(false)
+  dp[0] = true
   for (let i = 1; i <= n; i++) {
     for (let j = 0; j < i; j++) {
       if (dp[j] && set.has(s.substr(j, i - j))) {
-        dp[i] = true;
-        break;
+        dp[i] = true
+        break
       }
     }
   }
-  return dp[n];
-};
+  return dp[n]
+}
